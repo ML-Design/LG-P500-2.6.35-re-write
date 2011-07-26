943afd2b9b34691e7ad59397120621254ec4b873
francisco <francisco@francisco.(none)>
2011-07-26 04:37:13 +0100

    Revert "Squashed commits for 2.6.35.11"
    This reverts commit d2ab520133b385306599481c02fb6d96a8814790.

--- drivers/usb/host/xhci-mem.c
+++ drivers/usb/host/xhci-mem.c
@@ -1670,10 +1670,6 @@ static void xhci_add_in_port(struct xhci_hcd *xhci, unsigned int num_ports,
 				xhci->port_array[i] = (u8) -1;
 			}
 			/* FIXME: Should we disable the port? */
-<<<<<<< HEAD
-=======
-			continue;
->>>>>>> 0b06efa... Squashed commits for 2.6.35.11
 		}
 		xhci->port_array[i] = major_revision;
 		if (major_revision == 0x03)
@@ -1752,7 +1748,6 @@ static int xhci_setup_port_arrays(struct xhci_hcd *xhci, gfp_t flags)
 			return -ENOMEM;
 
 		port_index = 0;
-<<<<<<< HEAD
 		for (i = 0; i < num_ports; i++)
 			if (xhci->port_array[i] != 0x03) {
 				xhci->usb2_ports[port_index] =
@@ -1763,22 +1758,6 @@ static int xhci_setup_port_arrays(struct xhci_hcd *xhci, gfp_t flags)
 						xhci->usb2_ports[port_index]);
 				port_index++;
 			}
-=======
-		for (i = 0; i < num_ports; i++) {
-			if (xhci->port_array[i] == 0x03 ||
-					xhci->port_array[i] == 0 ||
-					xhci->port_array[i] == -1)
-				continue;
-
-			xhci->usb2_ports[port_index] =
-				&xhci->op_regs->port_status_base +
-				NUM_PORT_REGS*i;
-			xhci_dbg(xhci, "USB 2.0 port at index %u, "
-					"addr = %p\n", i,
-					xhci->usb2_ports[port_index]);
-			port_index++;
-		}
->>>>>>> 0b06efa... Squashed commits for 2.6.35.11
 	}
 	if (xhci->num_usb3_ports) {
 		xhci->usb3_ports = kmalloc(sizeof(*xhci->usb3_ports)*
